ef4780b89e3fcf35a66de26d85e172c96882019f
Mark Chmarny <mark@chmarny.com>
2026-03-30 07:31:23 -0700

    fix: increase admin service memory to 512Mi minimum

--- infra/saas/cloudrun.tf
+++ infra/saas/cloudrun.tf
@@ -261,7 +261,7 @@ resource "google_cloud_run_v2_service" "admin" {
       resources {
         limits = {
           cpu    = "1000m"
-          memory = "256Mi"
+          memory = "512Mi"
         }
       }
 
